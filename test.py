1617eb99372b4edf7b6f941a3a6fec1481b090ff
Craven <craven@milvue.com>
2022-12-27 23:37:35 +0100

    First version with 0.91 accuracy, to improve

---
+++ test.py
@@ -0,0 +1,12 @@
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys,os
+from importlib import reload
+
+print('ok')
